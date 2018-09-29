f90ad7d6d64e607c12deadb308f3b4dae0b45d16
Meliodas <erencorn@gmail.com>
2018-09-30 01:08:32 +0200

    Big boi commit

--- static/licenseheader.js
+++ static/licenseheader.js
@@ -1,8 +1,8 @@
 /*!
- * You are currently viewing the Ripple JavaScript source code.
- * Just so you know: if you want to see the source of the Ripple Frontend
- * (Hanayo), you can do so at https://zxq.co/ripple/hanayo
+ * You are currently viewing the Mansions JavaScript source code.
+ * Just so you know: if you want to see the source of the Mansions Frontend
+ * (Hanayo), you can do so at https://github.com/Mansions/hanayo
  * Un-minified version of this file as a whole is not available, however
- * its dependencies (jquery, timeago, semantic...) are open source, and ripple's
+ * its dependencies (jquery, timeago, semantic...) are open source, and Mansions's
  * custom JS is available at /static/ripple.js.
  */
